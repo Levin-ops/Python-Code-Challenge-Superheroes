669b4d717331c02cf9dc168d5f80869f763bccac
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2024-01-14 10:37:24 +0300

    get heroes

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Hero, Power
@@ -18,6 +18,22 @@ def home():
     return ('''<h1> Phase 4 Code Challenge 1</h1>
                 <h2>Superheroes</h2>''')
 
+@app.route('/heroes')
+def get_heroes():
+    heroes = Hero.query.all()
+
+    formated_output=[
+        {"id":hero.id,
+         "name":hero.name,
+         "super_name":hero.super_name}
+         for hero in heroes]
+    
+    response = make_response(
+        jsonify(formated_output),
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug = True)
